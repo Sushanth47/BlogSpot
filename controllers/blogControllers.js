f094b26067e0c18011ce7f17cf7d3717cf0fa9de
Sushanth47 <69962622+Sushanth47@users.noreply.github.com>
2021-10-14 19:02:08 +0530

    Done

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -1,47 +1,48 @@
 // blog_index, blog_detailes, blog_create_get, blog_create_post, blog_delete
-const Blog = require('../models/blog');
-
+const Blog = require("../models/blog");
+//hey
 
 const blog_index = async (req, res) => {
-   var result =await Blog.find().sort({ createdAt: -1});
-    return res.render('blogs/index', { title: 'All Blogs', blogs: result})  
-}
+  var result = await Blog.find().sort({ createdAt: -1 });
+  return res.render("blogs/index", { title: "All Blogs", blogs: result });
+};
 
-const blog_details = async (req, res) =>{
-    const id = req.params.id;
-    var blogFind = await Blog.findById(id)
-    if(blogFind){
-    return res.render('blogs/details', { blog: blogFind, title: 'Blog Details' })
-    }else{
-    res.status(404).render('404', {title: 'Blog not found'})
-    }
-    
-}
+const blog_details = async (req, res) => {
+  const id = req.params.id;
+  var blogFind = await Blog.findById(id);
+  if (blogFind) {
+    return res.render("blogs/details", {
+      blog: blogFind,
+      title: "Blog Details",
+    });
+  } else {
+    res.status(404).render("404", { title: "Blog not found" });
+  }
+};
 
-const blog_create_get = async (req, res) =>{
-    return res.render('blogs/create', { title: 'Create a new Blog' });
-}
+const blog_create_get = async (req, res) => {
+  return res.render("blogs/create", { title: "Create a new Blog" });
+};
 
 const blog_create_post = async (req, res) => {
-    const blog = new Blog(req.body);
+  const blog = new Blog(req.body);
 
-  await blog.save()
-   return res.redirect('/blogs');
-   
-}
+  await blog.save();
+  return res.redirect("/blogs");
+};
 
-const blog_delete = async (req, res) =>{
-    const id = req.params.id;
+const blog_delete = async (req, res) => {
+  const id = req.params.id;
 
-   await Blog.findByIdAndDelete(id)
+  await Blog.findByIdAndDelete(id);
 
-   return res.json({ redirect: '/blogs' })
-}
+  return res.json({ redirect: "/blogs" });
+};
 
 module.exports = {
-    blog_index,
-    blog_details,
-    blog_create_get,
-    blog_create_post,
-    blog_delete
-}
+  blog_index,
+  blog_details,
+  blog_create_get,
+  blog_create_post,
+  blog_delete,
+};
