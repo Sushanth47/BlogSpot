2dbcc23e5ecc8c0447cb2e9c827b47852dcdf700
Sushanth47 <69962622+Sushanth47@users.noreply.github.com>
2021-10-14 18:59:51 +0530

    Done

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,19 +1,16 @@
-const express = require('express');
-const blogController = require('../controllers/blogControllers')
-
+const express = require("express");
+const blogController = require("../controllers/blogControllers");
+console.log("hey");
 const router = express.Router();
 
-router.get('/', blogController.blog_index);
-  
-router.post('/', blogController.blog_create_post);
+router.get("/", blogController.blog_index);
 
-router.get('/create', blogController.blog_create_get);
+router.post("/", blogController.blog_create_post);
 
-router.get('/:id', blogController.blog_details)
+router.get("/create", blogController.blog_create_get);
 
-router.delete('/:id' , blogController.blog_delete)
+router.get("/:id", blogController.blog_details);
 
-
+router.delete("/:id", blogController.blog_delete);
 
 module.exports = router;
-  
